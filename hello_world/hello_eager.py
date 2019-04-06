2adb45980297663b07186613cac569dfb7083fe6
Allen Lu <allenlu2009@gmail.com>
2019-04-06 11:15:25 +0800

    Add eager and tf2 modes in py

---
+++ hello_world/hello_eager.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+tf.enable_eager_execution()
+
+# Create TensorFlow object called hello_constant
+hello_constant = tf.constant('Hello World!')
+
+print(hello_constant)
+
+    
---
+++ hello_world/hello_tf2.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+# Create TensorFlow object called hello_constant
+hello_constant = tf.constant('Hello World!')
+
+print(hello_constant)
+
+    
