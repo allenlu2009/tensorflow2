0ee54831aa221ba284e300e797db1a072b26d639
ALU <allenlu2009@gmail.com>
2019-04-13 21:09:58 +0800

    Add linear regression and mnist TF2.0 files
    linear_regression: tf2
    mnist: mnist_tf2 and fashion_mnist_tf2

---
+++ linear_regression/lin_reg_tf2.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 1. Generate data
+points = 500
+X = np.linspace(-10, 10, points)
+np.random.shuffle(X)
+Y = 2.0*X + 3.0 + np.random.normal(0, 1, (points, ))
+x_train, y_train = X[int(points*0.9):], Y[int(points*0.9):]
+x_test, y_test = X[:int(points*0.1)], Y[:int(points*0.1)]
+
+# 2. Build model
+model = tf.keras.models.Sequential(
+    [tf.keras.layers.Dense(units=1, input_shape=[1])])
+
+# 3. Train model with data
+model.compile(optimizer='sgd', loss='mse')
+model.fit(x_train, y_train, epochs=100)
+
+# 4. Test model with data
+model.evaluate(x_test, y_test)
+
+# 5. Predict/Infer data
+y_pred = model.predict(x_test)
+#print(y_pred)
+
+plt.scatter(x_test, y_test, c='r')
+#plt.scatter(x_test, y_pred, c='g')
+plt.plot(x_test, y_pred)
+plt.grid()
+plt.title('Linear Regression')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.axis([-10, 10, -20, 25])
+plt.show()
