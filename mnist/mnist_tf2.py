cff48d4666f8fe4959288c4c85eba2a382ec9c0f
ALU <allenlu2009@gmail.com>
2019-04-14 22:53:21 +0800

    Add Makefile and modify mnist_tf2

--- mnist/mnist_tf2.py
+++ mnist/mnist_tf2.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 mnist = tf.keras.datasets.mnist
 
@@ -12,6 +13,7 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=[28,28]),
     tf.keras.layers.Dense(units=128, activation='relu'),
     tf.keras.layers.Dense(units=num_classes, activation='softmax')])
+model.summary()
 
 # 3. train model with data
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
@@ -22,4 +24,11 @@ model.evaluate(test_images, test_labels)        # evaluate = test
 
 # 5. infer/predict use trained model
 pred_labels = model.predict(test_images)  # predict is for 1 test
-print(pred_labels)
+#print(pred_labels)
+
+# 6. save/restore model and check the accuracy 
+model.save('mnist_model.h5')
+new_model = tf.keras.models.load_model('mnist_model.h5')
+new_pred_labels = new_model.predict(test_images)
+np.testing.assert_allclose(pred_labels, new_pred_labels, atol=1e-6)
+
